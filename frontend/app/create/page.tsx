b158e2e34902cae0c91623d8ad7944a6bb836166
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-11-21 16:45:02 +0100

    add formÄ

--- frontend/app/create/page.tsx
+++ frontend/app/create/page.tsx
@@ -1,5 +1,12 @@
-export default function Create(){
+import Link from "next/link";
+import Form from "../components/Form";
 
+export default function CreateProduct() {
+  return (
+    <>
+      <h2>Add Product</h2>
+      <Link href="/">← Home</Link>
+      <Form />
+    </>
+  );
 }
-
-
