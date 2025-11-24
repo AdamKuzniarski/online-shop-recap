3c11e78d06cc3452ae598a3e82e180c962de77e1
Julius von Bonin <167006924+Tolpius@users.noreply.github.com>
2025-11-24 10:59:02 +0100

    change in eslint config

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -27,9 +27,11 @@ export default tseslint.config(
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-floating-promises': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
